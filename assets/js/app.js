08eb0c018095024b6b9f7b8a4babd93d698c95b8
Anushka Chouhan <157525924+AnushkaChouhan25@users.noreply.github.com>
2024-06-16 14:13:42 +0530

    app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -214,7 +214,7 @@ let footer = $(`
     </svg>
     </a>
 
-    <a class="social-button linkedin" href="www.linkedin.com/in/anushka-chouhan" target="_blank" >
+    <a class="social-button linkedin" href="https://www.linkedin.com/in/anushka-chouhan/" target="_blank" >
     <!-- svg code for linkedin icon -->
       <svg class="linkedin-icon-footer" xmlns="http://www.w3.org/2000/svg" width="35" viewBox="0 0 24 24" fill="#0e76a8"
         stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
